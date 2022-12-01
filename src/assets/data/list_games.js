3436a1d1d1bd4301fec9e114bd06e8968e89fa7a
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-01 16:12:59 +0700

    games: Add games list

---
+++ src/assets/data/list_games.js
@@ -0,0 +1,134 @@
+const list_games = [
+    {
+        id: 1,
+        title: 'Larger number',
+        desc: 'Compare the numbers and choose the larger one.'
+    },
+    {
+        id: 2,
+        title: 'Moving point',
+        desc: 'Watch the point and react to its color change.'
+    },
+    {
+        id: 3,
+        title: 'Unique figure',
+        desc: 'Find the same figures.'
+    },
+    {
+        id: 4,
+        title: 'Find number',
+        desc: 'Find the required number in the table.'
+    },
+    {
+        id: 5,
+        title: 'Squares number',
+        desc: 'Count the squares of a certain color.'
+    },
+    {
+        id: 6,
+        title: 'Position change',
+        desc: 'Count the number of points that haven\'t changed their position.'
+    },
+    {
+        id: 7,
+        title: 'Same image',
+        desc: 'Find the same image.'
+    },
+    {
+        id: 8,
+        title: 'Ascending numbers',
+        desc: 'Remember and find numbers in ascending order.'
+    },
+    {
+        id: 9,
+        title: 'Equal cells',
+        desc: 'find identical cells.'
+    },
+    {
+        id: 10,
+        title: 'Schulte table',
+        desc: 'Find numbers in the table.'
+    },
+    {
+        id: 11,
+        title: 'Swipe',
+        desc: 'Swipe in the right direction.'
+    },
+    {
+        id: 12,
+        title: 'Even-Odd',
+        desc: 'Choose the correct number.'
+    },
+    {
+        id: 13,
+        title: 'Spatial Imagination',
+        desc: 'Compare schemas.'
+    },
+    {
+        id: 14,
+        title: 'Excess cells',
+        desc: 'Remove excess cells.'
+    },
+    {
+        id: 15,
+        title: 'Number selection',
+        desc: 'Select the right number.'
+    },
+    {
+        id: 16,
+        title: 'Memory',
+        desc: 'Memorize positions.'
+    },
+    {
+        id: 17,
+        title: 'Countdown',
+        desc: 'Follow the countdown.'
+    },
+    {
+        id: 18,
+        title: 'Rotation',
+        desc: 'Find the points after the rotation.'
+    },
+    {
+        id: 19,
+        title: 'Math',
+        desc: 'Check the math equation.'
+    },
+    {
+        id: 20,
+        title: 'Visual coordination',
+        desc: 'Click on the dot at the intersection.'
+    },
+    {
+        id: 21,
+        title: 'Dots count',
+        desc: 'Count all dots on the screen.'
+    },
+    {
+        id: 22,
+        title: 'Reverse Schulte table',
+        desc: 'Find numbers in the table in reverse order.'
+    },
+    {
+        id: 23,
+        title: 'Memory matrix',
+        desc: 'Remember and find the right cells.'
+    },
+    {
+        id: 24,
+        title: 'Detect direction',
+        desc: 'Find the direction that most of the arrows are pointing.'
+    },
+    {
+        id: 25,
+        title: 'New figure',
+        desc: 'Find a new figure in the table.'
+    },
+    {
+        id: 26,
+        title: 'Same digit',
+        desc: 'Find all the numbers that have a certain digit.'
+    },
+]
+
+export default list_games;
