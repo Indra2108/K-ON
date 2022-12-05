50a972e5f11aefa9c63aafb6fa4d3112bfc52a4e
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-05 21:01:11 +0700

    screens: Add games, main, settings screens

---
+++ src/screens/main/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, View, FlatList } from 'react-native';
+
+import MainHeader from "../../component/header";
+import ListGames from "../../component/games_list";
+import list_games from "../../assets/data/list_games";
+
+function MainScreen() {
+    return (
+        <View style={styles.container}>
+            <MainHeader title='Exercises.' desc="Practice daily or when you can't concentrate and you will see improvement very soon." />
+
+            <FlatList
+                data={list_games}
+                renderItem={({ item }) => <ListGames id={item.id} title={item.title} desc={item.desc} />}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fbfaf5',
+        paddingHorizontal: '5%'
+    }
+})
+
+export default MainScreen;
