b09b8dbe5c15cac717f40d7e72ac08875390c565
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-06 17:56:41 +0700

    components: Update ListGames components

---
+++ src/components/listGames/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+import styles from "./styles";
+import ProgressBar from "react-native-progress/Bar";
+
+const ListGames = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.textId}>{props.id}. </Text>
+                <Text style={styles.textTitle}>{props.title}</Text>
+            </View>
+            <Text style={styles.textDesc}>{props.desc}</Text>
+            <ProgressBar
+                progress={props.progress || 0.1}
+                width={null}
+                height={4}
+                borderRadius={0}
+                borderWidth={0}
+                color='rgba(0, 0, 0, 1)'
+                unfilledColor='rgba(0, 0, 0, 0.3)'
+            />
+        </View>
+    )
+}
+
+export default ListGames;
