0a5ff987afc6f4110e75a6c465fac9a97a7e95f7
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-06 10:14:05 +0700

    screens: Add StatusBar and TopHeader components

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, StatusBar } from 'react-native';
 
+import TopHeader from "../../components/topHeader";
 import MainHeader from "../../components/header";
 import ListGames from "../../components/games_list";
 import list_games from "../../assets/data/list_games";
@@ -8,12 +9,17 @@ import list_games from "../../assets/data/list_games";
 function MainScreen() {
     return (
         <View style={styles.container}>
+            <StatusBar barStyle='dark-content' backgroundColor='#fbfaf5' />
+
+            <TopHeader name='gear' color='black'/>
+
             <MainHeader title='Exercises.' desc="Practice daily or when you can't concentrate and you will see improvement very soon." />
 
             <FlatList
                 data={list_games}
                 renderItem={({ item }) => <ListGames id={item.id} title={item.title} desc={item.desc} />}
                 keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
             />
         </View>
     )
@@ -23,6 +29,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fbfaf5',
+        paddingTop: '5%',
         paddingHorizontal: '5%'
     }
 })
