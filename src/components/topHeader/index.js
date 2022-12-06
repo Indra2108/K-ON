b97875db2e98b129f99df119c1b064c96d85e302
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-06 15:55:15 +0700

    components: Add naviation to topHeader component

--- src/components/topHeader/index.js
+++ src/components/topHeader/index.js
@@ -2,18 +2,26 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 import Icon from "react-native-vector-icons/EvilIcons";
+import { useNavigation } from '@react-navigation/native'
 
 const TopHeader = (props) => {
+    const navigation = useNavigation()
+
     return (
         <View style={styles.container}>
-            <Icon name={props.name} size={32} color={props.color} />
+            <Icon
+                name={props.name}
+                color={props.color}
+                size={30}
+                onPress={() => navigation.navigate(props.navigateTo)}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row-reverse'
+        alignSelf: 'flex-end',
     }
 })
 
