6b42f95231cff6721d7bc140574b9da2933d6871
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-12 10:49:07 +0700

    component: Add listGames navigation

--- src/components/listGames/index.js
+++ src/components/listGames/index.js
@@ -1,27 +1,33 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 
-import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 import ProgressBar from "react-native-progress/Bar";
 
+import styles from "./styles";
+
 const ListGames = (props) => {
+    const navigation = useNavigation()
+
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.textId}>{props.id}. </Text>
-                <Text style={styles.textTitle}>{props.title}</Text>
+        <TouchableWithoutFeedback onPress={() => navigation.navigate('GamesScreen')}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.textId}>{props.id}. </Text>
+                    <Text style={styles.textTitle}>{props.title}</Text>
+                </View>
+                <Text style={styles.textDesc}>{props.desc}</Text>
+                <ProgressBar
+                    progress={props.progress || 0.1}
+                    width={null}
+                    height={4}
+                    borderRadius={0}
+                    borderWidth={0}
+                    color='rgba(0, 0, 0, 1)'
+                    unfilledColor='rgba(0, 0, 0, 0.3)'
+                />
             </View>
-            <Text style={styles.textDesc}>{props.desc}</Text>
-            <ProgressBar
-                progress={props.progress || 0.1}
-                width={null}
-                height={4}
-                borderRadius={0}
-                borderWidth={0}
-                color='rgba(0, 0, 0, 1)'
-                unfilledColor='rgba(0, 0, 0, 0.3)'
-            />
-        </View>
+        </TouchableWithoutFeedback>
     )
 }
 
