f20ecdb8e2049479e5c9341cbaa5efb464103fb0
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-12 10:50:04 +0700

    component: Simplifying exporting module

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import TopHeader from "./topHeader";
+import MainHeader from "./header";
+import ListGames from "./listGames";
+import ListSettings from "./listSettings";
+import toastConfig from "./toast";
+
+
+export { TopHeader, MainHeader, ListGames, ListSettings, toastConfig };
