dc2e6a31abdb5e6a771af173b98de5aa804ebf28
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-14 22:22:17 +0700

    asset: data: Add howToPlay property

--- src/assets/data/list_games.js
+++ src/assets/data/list_games.js
@@ -2,132 +2,158 @@ const list_games = [
     {
         id: 1,
         title: 'Larger number',
-        desc: 'Compare the numbers and choose the larger one.'
+        desc: 'Compare the numbers and choose the larger one.',
+        howToPlay: 'Look at the center point, while choosing a larger number. And try to do it as quickly as possible'
     },
     {
         id: 2,
         title: 'Moving point',
-        desc: 'Watch the point and react to its color change.'
+        desc: 'Watch the point and react to its color change.',
+        howToPlay: ''
     },
     {
         id: 3,
         title: 'Unique figure',
-        desc: 'Find the same figures.'
+        desc: 'Find the same figures.',
+        howToPlay: ''
     },
     {
         id: 4,
         title: 'Find number',
-        desc: 'Find the required number in the table.'
+        desc: 'Find the required number in the table.',
+        howToPlay: ''
     },
     {
         id: 5,
         title: 'Squares number',
-        desc: 'Count the squares of a certain color.'
+        desc: 'Count the squares of a certain color.',
+        howToPlay: ''
     },
     {
         id: 6,
         title: 'Position change',
-        desc: 'Count the number of points that haven\'t changed their position.'
+        desc: 'Count the number of points that haven\'t changed their position.',
+        howToPlay: ''
     },
     {
         id: 7,
         title: 'Same image',
-        desc: 'Find the same image.'
+        desc: 'Find the same image.',
+        howToPlay: ''
     },
     {
         id: 8,
         title: 'Ascending numbers',
-        desc: 'Remember and find numbers in ascending order.'
+        desc: 'Remember and find numbers in ascending order.',
+        howToPlay: ''
     },
     {
         id: 9,
         title: 'Equal cells',
-        desc: 'find identical cells.'
+        desc: 'find identical cells.',
+        howToPlay: ''
     },
     {
         id: 10,
         title: 'Schulte table',
-        desc: 'Find numbers in the table.'
+        desc: 'Find numbers in the table.',
+        howToPlay: ''
     },
     {
         id: 11,
         title: 'Swipe',
-        desc: 'Swipe in the right direction.'
+        desc: 'Swipe in the right direction.',
+        howToPlay: ''
     },
     {
         id: 12,
         title: 'Even-Odd',
-        desc: 'Choose the correct number.'
+        desc: 'Choose the correct number.',
+        howToPlay: ''
     },
     {
         id: 13,
         title: 'Spatial Imagination',
-        desc: 'Compare schemas.'
+        desc: 'Compare schemas.',
+        howToPlay: ''
     },
     {
         id: 14,
         title: 'Excess cells',
-        desc: 'Remove excess cells.'
+        desc: 'Remove excess cells.',
+        howToPlay: ''
     },
     {
         id: 15,
         title: 'Number selection',
-        desc: 'Select the right number.'
+        desc: 'Select the right number.',
+        howToPlay: ''
     },
     {
         id: 16,
         title: 'Memory',
-        desc: 'Memorize positions.'
+        desc: 'Memorize positions.',
+        howToPlay: ''
     },
     {
         id: 17,
         title: 'Countdown',
-        desc: 'Follow the countdown.'
+        desc: 'Follow the countdown.',
+        howToPlay: ''
     },
     {
         id: 18,
         title: 'Rotation',
-        desc: 'Find the points after the rotation.'
+        desc: 'Find the points after the rotation.',
+        howToPlay: ''
     },
     {
         id: 19,
         title: 'Math',
-        desc: 'Check the math equation.'
+        desc: 'Check the math equation.',
+        howToPlay: ''
     },
     {
         id: 20,
         title: 'Visual coordination',
-        desc: 'Click on the dot at the intersection.'
+        desc: 'Click on the dot at the intersection.',
+        howToPlay: ''
     },
     {
         id: 21,
         title: 'Dots count',
-        desc: 'Count all dots on the screen.'
+        desc: 'Count all dots on the screen.',
+        howToPlay: ''
     },
     {
         id: 22,
         title: 'Reverse Schulte table',
-        desc: 'Find numbers in the table in reverse order.'
+        desc: 'Find numbers in the table in reverse order.',
+        howToPlay: ''
     },
     {
         id: 23,
         title: 'Memory matrix',
-        desc: 'Remember and find the right cells.'
+        desc: 'Remember and find the right cells.',
+        howToPlay: ''
     },
     {
         id: 24,
         title: 'Detect direction',
-        desc: 'Find the direction that most of the arrows are pointing.'
+        desc: 'Find the direction that most of the arrows are pointing.',
+        howToPlay: ''
     },
     {
         id: 25,
         title: 'New figure',
-        desc: 'Find a new figure in the table.'
+        desc: 'Find a new figure in the table.',
+        howToPlay: ''
     },
     {
         id: 26,
         title: 'Same digit',
-        desc: 'Find all the numbers that have a certain digit.'
+        desc: 'Find all the numbers that have a certain digit.',
+        howToPlay: ''
     },
 ]
 
