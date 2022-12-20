d6d23b77980d441b01b4c18257649f9f375c4dfe
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-20 21:13:58 +0700

    screen: games: major changes

--- src/screens/games/index.js
+++ src/screens/games/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableWithoutFeedback, } from 'react-native';
 
 import { useRoute } from "@react-navigation/native";
 
 import { TopHeader, MainHeader } from "../../components";
+import LargerNumberGame from "../../assets/games_components/01.Larger_Number";
 
 const GamesScreen = () => {
     const route = useRoute()
     const { title, desc } = route.params
+    const [startGame, setStartGame] = useState(false)
 
     return (
         <View style={styles.container}>
@@ -17,12 +19,29 @@ const GamesScreen = () => {
                 navigateTo='MainScreen'
             />
 
-            <MainHeader
-                style={{ marginTop: '2%', fontSize: 30, }}
-                title={title}
-                desc={desc}
-            />
-        </View>
+            {
+                startGame ?
+
+                    <View style={styles.viewTextStart}>
+                        <LargerNumberGame />
+                    </View>
+
+                    :
+
+                    <>
+                        <MainHeader
+                            style={{ marginTop: '2%', fontSize: 30, }}
+                            title={title}
+                            desc={desc}
+                        />
+                        <TouchableWithoutFeedback onPress={() => setStartGame(true)}>
+                            <View style={[styles.viewTextStart, { flex: 1, justifyContent: 'center', }]}>
+                                <Text style={styles.textStart}>Start</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </>
+            }
+        </View >
     )
 }
 
@@ -32,6 +51,17 @@ const styles = StyleSheet.create({
         padding: '5%',
         backgroundColor: '#fbfaf5',
     },
+    viewTextStart: {
+        flex: 1,
+    },
+    textStart: {
+        fontFamily: "Mplus1-Bold",
+        fontSize: 40,
+        color: 'black',
+        letterSpacing: -1,
+        textAlign: 'center'
+    },
+
 })
 
 export default GamesScreen;
