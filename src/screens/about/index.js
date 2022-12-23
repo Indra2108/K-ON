1bfc1a695f192f3b59bd22cd73871bd717987d05
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-24 06:54:54 +0700

    screen: Add About screen

---
+++ src/screens/about/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from 'react-native';
+
+const AboutScreen = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default AboutScreen
