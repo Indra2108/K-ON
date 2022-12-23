7ec57124b204bc36c2687a24d27e1a28dbb09210
Indra Damar Jati <fizipratama21@gmail.com>
2022-12-24 06:56:04 +0700

    route: Add About and Result route screen

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from "../screens/main";
 import SettingScreen from "../screens/settings";
 import GamesScreen from "../screens/games";
+import AboutScreen from "../screens/about";
+import ResultScreen from "../screens/result";
 
 const Router = () => {
     const Stack = createNativeStackNavigator();
@@ -16,6 +18,8 @@ const Router = () => {
                 <Stack.Screen name='MainScreen' component={MainScreen} />
                 <Stack.Screen name='GamesScreen' component={GamesScreen} />
                 <Stack.Screen name='SettingScreen' component={SettingScreen} />
+                <Stack.Screen name='AboutScreen' component={AboutScreen} />
+                <Stack.Screen name='ResultScreen' component={ResultScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
