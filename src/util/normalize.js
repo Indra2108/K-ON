1eed41560868c8b073c2c8cb62bfbe08c40d13da
Indra Damar Jati <fizipratama21@gmail.com>
2023-01-06 09:45:38 +0700

    util: Add normalize function to make responsive screen

---
+++ src/util/normalize.js
@@ -0,0 +1,20 @@
+import { Dimensions, Platform, PixelRatio } from 'react-native';
+
+const {
+  width: SCREEN_WIDTH,
+  height: SCREEN_HEIGHT,
+} = Dimensions.get('window');
+
+// based on iphone 5s's scale
+const scale = SCREEN_WIDTH / 320;
+
+function normalize(size) {
+  const newSize = size * scale
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize))
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+  }
+}
+
+export default normalize
