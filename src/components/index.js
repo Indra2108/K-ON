0364847b767e93b7d0c0beecc83ab439fc24de35
Indra Damar Jati <fizipratama21@gmail.com>
2023-01-09 10:01:01 +0700

    core: removing toastConfig

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,7 @@ import TopHeader from "./topHeader";
 import MainHeader from "./header";
 import ListGames from "./listGames";
 import ListSettings from "./listSettings";
-import toastConfig from "./toast";
 import Button from "./button";
 
 
-export { TopHeader, MainHeader, ListGames, ListSettings, toastConfig, Button };
+export { TopHeader, MainHeader, ListGames, ListSettings, Button };
